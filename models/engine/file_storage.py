e3dcce6567a5a08f7ddcce9d826f18fb0e0a6174
Yakub289 <Yakubsuleiman24@gmail.com>
2023-08-11 09:31:07 -0700

    Storage path

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,13 +6,6 @@
 import os
 import json
 import datetime
-from models.user import User
-from models.city import City
-from models.state import State
-from models.place import Place
-from models.review import Review
-from models.amenity import Amenity
-from models.base_model import BaseModel
 
 
 class FileStorage:
