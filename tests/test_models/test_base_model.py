ebd47f7fe31d5a028b8eb9fcf29c98ae3e327ab0
Yakub289 <Yakubsuleiman24@gmail.com>
2023-08-12 10:18:23 -0700

    Test cases

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-"""Defines unittests for models/base_model.py.
+
+
+"""
+Defines unittests for models/base_model.py.
 
 Unittest classes:
     TestBaseModel_instantiation
     TestBaseModel_save
     TestBaseModel_to_dict
 """
+
+
 import os
 import models
 import unittest
-from datetime import datetime
 from time import sleep
+from datetime import datetime
 from models.base_model import BaseModel
 
 
